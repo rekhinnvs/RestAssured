9b198c7babcce8bd2edc4b96f65dfe5cf1d043f2
rekhin.nvs <rekhin.nvs@gmail.com>
2020-07-21 10:08:01 +0530

    Update user using PUT method.

--- src/test/java/Reqres.java
+++ src/test/java/Reqres.java
@@ -1,3 +1,4 @@
+import io.restassured.path.json.JsonPath;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -13,9 +14,9 @@ public class Reqres {
         baseUrl = "https://reqres.in/";
     }
 
-    @Test
+    @Test(enabled = false,priority = 1)
     public void requestUsers() {
-        //Create a users with the given details
+        //Create a users with the given details and verify the status code
         endpoint = "api/users";
         given()
                 .header("Content-Type","application/json")
@@ -26,5 +27,41 @@ public class Reqres {
         .when().post(baseUrl+endpoint)
         .then().log().body()
                 .assertThat().statusCode(201);
+    }
+
+    @Test(priority = 2)
+    public void getAndUpdateUser() {
+        //Update and retrieve the values, [actually the values are not retained in the server]
+        endpoint = "api/users";
+        //Get the response in a string
+        String response = given()
+                            .header("Content-Type","application/json")
+                            .when().get(baseUrl+endpoint)
+                            .then().log().body().extract().response().asString();
+
+        //System.out.println(response);
+        System.out.println("************");
+        //Convert the string to json and get the value from it.
+        //JsonPath js = new JsonPath(response);
+        //System.out.println(js.getString("data.id"));
+
+        System.out.println("************");
+        //Update the user using PUT
+        given()
+                .header("Content-Type","application/json")
+                .body("{\n" +
+                        "    \"name\": \"morpheus\",\n" +
+                        "    \"job\": \"zion resident\"\n" +
+                        "}")
+                .when()
+                .put(baseUrl+endpoint)
+                .then();
+
+        //Get the value after updating.
+        given()
+                .header("Content-Type","application/json")
+                .when().get(baseUrl+"api/users")
+                .then().log().body().extract().response().asString();
+
     }
 }
