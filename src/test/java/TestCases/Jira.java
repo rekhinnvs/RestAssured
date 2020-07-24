7e65d8dfadd3afdd39a1e9ffc100e52115bc89c4
rekhin.nvs <rekhin.nvs@gmail.com>
2020-07-24 09:52:39 +0530

    Get custom fields from issue

--- src/test/java/TestCases/Jira.java
+++ src/test/java/TestCases/Jira.java
@@ -1,6 +1,8 @@
 package TestCases;
 
 import io.restassured.RestAssured;
+import io.restassured.internal.http.HTTPBuilder;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -9,6 +11,7 @@ import utils.Payload;
 import java.io.File;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.preemptive;
 
 public class Jira {
 
@@ -41,29 +44,46 @@ public class Jira {
 
     }
 
-    @Test(enabled = false)
+    @Test(enabled = false,priority = 3)
     public void addComment() {
         issueID = "CLAS-4";
         endpoint = "rest/api/2/issue/"+issueID+"/comment";
-
         int responseCode = given().auth().preemptive().basic("dracksagain@gmail.com","0BnHftVpB11ca6xMik0D5FA4")
                 .header("Content-Type","application/json")
                 .body(Payload.getnewComment())
+                .when()
                 .post(endpoint)
                 .then().log().body().log().status().extract().response().getStatusCode();
         System.out.println("Response code : " +responseCode);
     }
 
-    @Test
+    @Test(enabled = false,priority = 4)
     public void addAttachment() {
         issueID = "CLAS-4";
         endpoint = "rest/api/2/issue/"+issueID+"/attachments";
         given().auth().preemptive().basic("dracksagain@gmail.com","0BnHftVpB11ca6xMik0D5FA4")
                 .header("X-Atlassian-Token","no-check")
                 .multiPart("file",new File("./src/test/resources/bug_clas4"))
+                .when()
                 .post(endpoint)
-                .then().log().status();
+                .then().log().status().log().body();
     }
 
+    @Test(enabled = true,priority = 2)
+    public void getIssueFields() {
+        issueID = "CLAS-4";
+        endpoint = "rest/api/2/issue/"+issueID;
+        //Add the authentication globally so that we can avoid authenticating every time.
+        RestAssured.authentication = RestAssured.preemptive().basic("dracksagain@gmail.com","0BnHftVpB11ca6xMik0D5FA4");
 
+        Response response = given()
+                .when().get(endpoint)
+                .then().extract().response();
+        JsonPath js = response.jsonPath();
+        int numberOfComments = js.getInt("fields.comment.comments.size()");
+        //Get all the comments for the issue.
+        for(int i=0; i<numberOfComments; i++) {
+            System.out.println(js.getString("fields.comment.comments["+i+"].body"));
+        }
+    }
 }
